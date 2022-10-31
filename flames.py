edee0f4a95981bb634deaf70420bc39587808d0d
Vmkmanoj <116948057+Vmkmanoj@users.noreply.github.com>
2022-10-31 01:52:00 -0700

    Add files via upload

---
+++ flames.py
@@ -0,0 +1,24 @@
+#funny code
+a=input("Enter you name:")
+b=input("Enter your Friendname:")
+b=set(a)^set(b)
+d=len(b)
+if d==1:
+    print("affaction")
+if d==2:
+    print("enimy")
+if d==3:
+    print("marriage")
+if d==4:
+    print("enimy")
+if d==5:
+    print("frined")
+if d==6:
+    print("affaction")
+if d==7:
+    print("enimy")
+if d==8:
+    print("affaction")
+if d==9:
+    print("enimy")
+print(d)
